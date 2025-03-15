e7ef653136e461dd4ba26c711fef222cd0f28644
Henrique Siqueira <henrique.alves.siqueira@hotmail.com>
2025-03-14 22:37:23 -0600

    Fix: Host

--- portal_grs/portal_grs/settings.py
+++ portal_grs/portal_grs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%pyujf0qts@ihh=p9gpm$&r&-3&caa#l_#35aa=a1yp1k(#*=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portal-grs.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
