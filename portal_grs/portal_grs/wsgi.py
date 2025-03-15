4ec5a32862fe61a8770e902966704403db5b5b30
Henrique Siqueira <henrique.alves.siqueira@hotmail.com>
2025-03-14 22:21:14 -0600

    Last try Fix deploy

--- portal_grs/portal_grs/wsgi.py
+++ portal_grs/portal_grs/wsgi.py
@@ -1,4 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal_grs.settings')
 application = get_wsgi_application()
+
